8acff369cd02ca4f79bf264126685e642adf96a9
Andrew <andrewvu.cs@gmail.com>
2020-02-19 14:11:46 -0800

    wrote more test

--- script.test.js
+++ script.test.js
@@ -4,12 +4,28 @@ const googleSearch = require("./script");
 
 dbMock = ["dog.com", "cheespuff.com", "disney.com", "dogpics.com"];
 
-it("it is a sample test", () => {
-  expect("hello").toBe("hello");
-});
 
-it("is searching google", () => {
-    // To Equal an Empty Array
-  expect(googleSearch("testtest", dbMock)).toEqual([]);
-  expect(googleSearch("dog", dbMock)).toEqual(['dog.com', 'dogpics.com']);
-});
+// describe, Grouping tests that are similar. Easier to navigate tests
+describe('googleSearch', () => {
+
+    it("it is a sample test", () => {
+        expect("hello").toBe("hello");
+      });
+      
+      it("is searching google", () => {
+          // To Equal an Empty Array
+        expect(googleSearch("testtest", dbMock)).toEqual([]);
+        expect(googleSearch("dog", dbMock)).toEqual(['dog.com', 'dogpics.com']);
+      });
+      
+      
+      // Testing out undefined and null 
+      it('work with undefined and null input', () => {
+          expect(googleSearch(undefined, dbMock)). toEqual([]);
+          expect(googleSearch(null, dbMock)). toEqual([]);
+      })
+      
+      it('does not return more than 3 matches', () => {
+          expect(googleSearch('.com', dbMock).length).toEqual(3);
+      })
+})
