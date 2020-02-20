31837f641431df4f01454d4920a24829f7216f31
Andrew <andrewvu.cs@gmail.com>
2020-02-19 16:41:41 -0800

    created more async test, worked with jest 'done' param and expect assertions

--- script2.test.js
+++ script2.test.js
@@ -1,3 +1,34 @@
 const fetch = require('node-fetch');
 const swapi = require('./script2');
 
+it ('calls swapi to get people', (done) => {
+    // wrong way, believes the async code is finished
+    expect.assertions(1);
+    swapi.getPeople(fetch).then(data => {
+        expect(data.count).toEqual(87);
+        // pass done in the param to see when the async value is done
+        done();
+    })
+})
+
+// When running ASYNC test always implement expect assertions!
+it ('calls swapi to get people', () => {
+    expect.assertions(1);
+    return swapi.getPeople(fetch).then(data => {
+        expect(data.count).toEqual(87);
+    })
+})
+
+it ('calls swapi to get people', () => {
+    expect.assertions(2);
+    return swapi.getPeople(fetch).then(data => {
+        expect(data.count).toEqual(87)
+        expect(data.results.length).toBeGreaterThan(3);
+    })
+})
+
+it ('calls swapi to get people with a promise', () => {
+    swapi.getPeoplePromise(fetch).then(data => {
+        expect(data.count).toEqual(87);
+    })
+})
